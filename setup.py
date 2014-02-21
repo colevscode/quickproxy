b6d18731a22a7b14fff962f9c7f4dabb22f64e41
Cole Krumbholz <colevscode@Coles-MacBook-Air.local>
2014-02-20 21:35:15 -0800

    bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ root = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name='quickproxy',
-    version='0.1.1',
+    version='0.2.0',
     author='Cole Krumbholz',
     author_email='cole@brace.io',
     description='A lightweight, per-request customizable HTTP proxy for python.',
