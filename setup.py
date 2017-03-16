c7c774c914528931a6d6e7f629e54a8e824e856b
Sivabudh Umpudh <sivabudh.u@codium.co>
2017-03-16 17:39:46 +0700

    Made the project Python 3.6 compatible.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 root = os.path.dirname(os.path.realpath(__file__))
 
 setup(
-    name='quickproxy',
-    version='0.2.0',
+    name='quickproxy3',
+    version='0.3.0',
     author='Cole Krumbholz',
     author_email='cole@brace.io',
     description='A lightweight, per-request customizable HTTP proxy for python.',
-    packages=['quickproxy'],
-    install_requires=open(root+"/requirements.txt").read().splitlines(),
-    long_description=open(root+"/README.md").read(),
+    packages=['quickproxy3'],
+    install_requires=open(root + "/requirements.txt").read().splitlines(),
+    long_description=open(root + "/README.md").read(),
     license='LICENSE',
-    package_data={'quickproxy': ['data/test.*']},
+    package_data={'quickproxy3': ['data/test.*']},
 )
